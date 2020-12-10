d990374e2d81ce104810612b558211cb8f5a384b
sebaszap123 <mfrausto0@ucol.mx>
2020-12-10 03:47:46 -0600

    Correction of presentation and sumarParesDo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
-export default class App {}
+export default class App {
+  presentacion() {
+    for (let i = 5; i <= 25; i = i + 5) {
+      console.log(i);
+    }
+  }
+ /* sumarParesFor() {
+   
+  } */ /* Esta mal*/
+  sumarParesDo() {
+    let i = 1;
+    let suma = 0;
+    do {
+      if(i%2 === 0) {
+        suma = suma + i;
+      }
+      i++;
+    } while (i <= 20);
+
+    return suma
+  }
+}
 
 let app = new App();
+
+console.log("demo de función for");
+app.presentacion();
+console.log(app.sumarParesDo());
+
