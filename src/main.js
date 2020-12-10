3cd792f793be8d91ba632016fe408a13cc4cad92
sebaszap123 <mfrausto0@ucol.mx>
2020-12-10 05:53:51 -0600

    All Right corrections and added function contarImparesDo

--- src/main.js
+++ src/main.js
@@ -14,17 +14,40 @@ export default class App {
     }
     return suma;
   }
-
-  contarImparesFor(inicio, fin){
+  contarImparesFor(inicio, fin) {
     let impares = 0;
-    for(let i = inicio; i <= fin; i = i + 1) {
-      if( i%2 !== 0) {
-        impares = impares + 1
+    for (let i = inicio; i <= fin; i = i + 1) {
+      if (i % 2 !== 0) {
+        impares = impares + 1;
       }
     }
     return impares;
   }
+  sumarParesWhile() {
+    let i = 1;
+    let suma = 0;
 
+    while (i <= 20) {
+      if (i % 2 === 0) {
+        suma = suma + i;
+      }
+      i = i + 1;
+    }
+    return suma;
+  }
+
+  contarImparesWhile(inicio, fin) {
+    let i = inicio;
+    let impares = 0;
+
+    while (i <= fin) {
+      if (i % 2 !== 0) {
+        impares++;
+      }
+      i++;
+    }
+    return impares;
+  }
 
   sumarParesDo() {
     let i = 1;
@@ -38,6 +61,24 @@ export default class App {
 
     return suma;
   }
+
+  contarImparesDo(inicio, fin) {
+    if (inicio > fin) {
+      let t = inicio;
+      inicio = fin;
+      fin = t;
+    }
+    let i = inicio;
+    let impares = 0;
+
+    do {
+      if (i % 2 !== 0) {
+        impares++;
+      }
+      i++;
+    } while (i <= fin);
+    return impares;
+  }
 }
 
 let app = new App();
@@ -50,6 +91,10 @@ console.log(app.sumarParesFor());
 
 console.log(app.contarImparesFor());
 
+console.log(app.sumarParesWhile());
+
+console.log(app.contarImparesWhile(5, 11));
+
 console.log(app.sumarParesDo());
 
-
+console.log(app.contarImparesDo(11, 5));
